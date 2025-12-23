5281330e819fee955fe7df27a2b8eec050b6cedb
Jackson Vanover <jacksonvanover@gmail.com>
2025-12-23 12:13:57 -0800

    latex equation fix

--- stage_blog_post.sh
+++ stage_blog_post.sh
@@ -20,6 +20,7 @@ sed -Ei 's|^!\[\[([^]]+)\]\]|![](/assets/img/\1)|' docs/_posts/"${TO_FILENAME}"
 
 # fix latex equations
 sed -i 's|\$|\$\$|g' docs/_posts/"${TO_FILENAME}"
+sed -i 's|\$\$\$\$|\$\$|g' docs/_posts/"${TO_FILENAME}"
 
 # copy over images
 grep -oe "/assets/img/.*png" docs/_posts/"${TO_FILENAME}" | while read x
